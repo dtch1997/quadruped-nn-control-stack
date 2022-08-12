873accc582bd95a1fc86c9991127de0ea391d562
dtch1997 <dtch1997@users.noreply.github.com>
2022-08-12 13:19:25 +0800

    Delete old code

--- include/joystick_comm.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-typedef struct
-	{
-		float joyLeftAnalog[2];               // Left Analog Stick - Vel cmd
-        float joyRightAnalog[2];              // Right Analog Stick - Direction cmd
-	} JoystickCmd;   
